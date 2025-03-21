785e33fddb9315ca292adda53cff6c78552ddbca
ss3468 <ss3468@njit.edu>
2025-03-20 20:04:51 -0400

    tem

---
+++ house-prices-advanced-regression-techniques/pre_imputer/.ipynb_checkpoints/Lotfrontage_impute-checkpoint.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+class Lfrontage:
+    def __init__(self):
+        self.df_group_1=None
+        self.df_group_2=None
+        self.df_group_3=None
+        self.df_group_4=None
+        self.df_group_broad=None
+        self.bin_edges=None
+        self.lotfrontage_std=0
+    def fit(self,df):
+        data=df[['LotArea','Neighborhood','LotShape','LotConfig','LotFrontage']].copy()
+        data['LotArea_transformed']=np.log1p(data['LotArea'])
+        n = len(data['LotArea_transformed'])
+        self.lotfrontage_std=data['LotFrontage'].std()
+        IQR = np.percentile(data['LotArea_transformed'], 75) - np.percentile(data['LotArea_transformed'], 25)
+        bin_width_sqrt = 2 * IQR / (n ** (1 / 3))
+        num_bins_sqrt = int(np.ceil((data['LotArea_transformed'].max() - data['LotArea_transformed'].min()) / bin_width_sqrt))
+        self.bin_edges = np.linspace(data['LotArea_transformed'].min(), data['LotArea_transformed'].max(), num_bins_sqrt + 1).tolist()
+        self.bin_edges=np.append([-np.inf],self.bin_edges).tolist()
+        self.bin_edges.append(np.inf)
+        data['LotArea_Bins'] = pd.cut(data['LotArea_transformed'], bins=self.bin_edges,labels=[f"Bin {i+1}" for i in range(len(self.bin_edges) - 1)])
+        self.df_group_1=data.groupby(['Neighborhood','LotShape','LotConfig','LotArea_Bins'])['LotFrontage'].median().reset_index()
+        self.df_group_2=data.groupby(['Neighborhood','LotShape','LotConfig'])['LotFrontage'].median().reset_index()
+        self.df_group_3=data.groupby(['Neighborhood','LotShape'])['LotFrontage'].median().reset_index()
+        self.df_group_4=data.groupby(['Neighborhood','LotConfig'])['LotFrontage'].median().reset_index()
+        self.df_group_broad=data.groupby(['Neighborhood'])['LotFrontage'].median().reset_index()
+        upper_bound_original = (self.bin_edges[-2]) ** 2
+        # print(f"Upper bound for last bin (original scale): {upper_bound_original}")
+    def transform(self,df):
+        if(self.df_group_1.empty)|(self.df_group_2.empty)|(self.df_group_3.empty):
+            raise ValueError("df_group not fitted")
+        else:
+            df['LotArea_transformed']=np.log1p(df['LotArea'])
+            df['LotArea_Bins'] = pd.cut(df['LotArea_transformed'], bins=self.bin_edges,labels=[f"Bin {i}" for i in range(len(self.bin_edges) - 1)],right=False)
+            # df['LotArea_Bins']=np.where(df['LotArea_Bins'].isnull()&(df['LotArea_transformed']<=self.bin_edges[0]),"Bin 0",df['LotArea_Bins'])
+            # print([f"Bin {i}" for i in range(len(self.bin_edges) - 1)])
+            noise_mean = 0       # Centered at 0
+            noise_std = 0.1*self.lotfrontage_std      # Small standard deviation
+            noise = np.random.normal(noise_mean, noise_std, size=len(df))
+            df=pd.merge(df, self.df_group_1, on=['Neighborhood','LotShape','LotConfig','LotArea_Bins'], how='left', suffixes=('', '_1'))
+            df['LotFrontage']=np.where(df['LotFrontage'].isnull(),round(df['LotFrontage_1']+noise,2),df['LotFrontage'])
+            df=pd.merge(df, self.df_group_2, on=['Neighborhood','LotShape','LotConfig'], how='left', suffixes=('', '_2'))
+            df['LotFrontage']=np.where(df['LotFrontage'].isnull(),round(df['LotFrontage_2']+noise,2),df['LotFrontage'])
+            df=pd.merge(df, self.df_group_3, on=['Neighborhood','LotShape'], how='left', suffixes=('', '_3'))
+            df['LotFrontage']=np.where(df['LotFrontage'].isnull(),round(df['LotFrontage_3']+noise,2),df['LotFrontage'])
+            df=pd.merge(df, self.df_group_4, on=['Neighborhood','LotConfig'], how='left', suffixes=('', '_4'))
+            df['LotFrontage']=np.where(df['LotFrontage'].isnull(),round(df['LotFrontage_4']+noise,2),df['LotFrontage'])
+            df=pd.merge(df, self.df_group_broad, on=['Neighborhood'], how='left', suffixes=('', '_broad'))
+            df['LotFrontage']=np.where(df['LotFrontage'].isnull(),round(df['LotFrontage_broad']+noise,2),df['LotFrontage'])
+            df.drop(columns=['LotArea_transformed','LotArea_Bins','LotFrontage_1','LotFrontage_2','LotFrontage_3','LotFrontage_4','LotFrontage_broad'], inplace=True)
+        return df
+    def fit_transform(self,df):
+        self.fit(df)
+        return self.transform(df)
